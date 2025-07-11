1d548873e073f72061e1c93cf51a226aabc9f691
adomaitisc <caadomaitis@gmail.com>
2025-07-10 23:56:53 -0400

    okok

--- src/components/ui/phone.tsx
+++ src/components/ui/phone.tsx
@@ -41,7 +41,7 @@ export function Phone({
           height={40}
           className="absolute bottom-0 left-0 pointer-events-none z-10"
         />
-        {children}
+        <div className="absolute inset-0 bg-white pt-12 pb-10">{children}</div>
       </div>
     </motion.div>
   );
